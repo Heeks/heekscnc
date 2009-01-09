a1fbca42e799f2ba3b7d0ffc646ca9758b6bacf3
danheeks <danheeks@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-01-09 13:01:32 +0000

    added extract of permission email from Anders

--- trunk/src/DropCutter.h
+++ trunk/src/DropCutter.h
@@ -4,6 +4,19 @@
 // written by Anders Wallin as DropCutter.cs
 // converted to C++ by Dan Heeks starting on May 2nd 2008
 
+// extract of email 2nd July 2008
+
+// Dan Heeks said:
+//	Anders,
+//    I have copied your DropCutter.cs for calculating the Z height from triangles.
+//    I would like to use this in an open source project, but using a more permissive license than you.
+//    The files I have made, derived from yours, are DropCutter.h and DropCutter.cpp, I have attached them.
+//    I had to add tolerances to the tests.
+//    Please can you give me permission to use this copy and release it under a BSD license?
+//
+// Anders Wallin said:
+// yes, you are free to release this under the BSD license if you want. As someone pointed out on my blog the edge-test for the toroidal cutter is wrong, or at least only an approximation to the exact geometry
+
 class Cutter{
 public:
 	double R; // shaft radius
