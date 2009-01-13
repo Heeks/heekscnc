70517fd81abe55a8a6964dea86e2e4caddca2880
danheeks <danheeks@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-01-13 14:41:30 +0000

    done: Issue 10:  	 NC Code window, graphics, interactive

--- trunk/src/OutputCanvas.cpp
+++ trunk/src/OutputCanvas.cpp
@@ -2,18 +2,40 @@
 
 #include "stdafx.h"
 #include "OutputCanvas.h"
+#include "Program.h"
+#include "NCCode.h"
+
+BEGIN_EVENT_TABLE(COutputTextCtrl, wxTextCtrl)
+    EVT_MOUSE_EVENTS(COutputTextCtrl::OnMouse)
+END_EVENT_TABLE()
+
+
+void COutputTextCtrl::OnMouse( wxMouseEvent& event )
+{
+	if(event.LeftUp())
+	{
+		wxTextPos pos = GetInsertionPoint();
+		if(theApp.m_program && theApp.m_program->m_nc_code)
+		{
+			theApp.m_program->m_nc_code->HighlightBlock(pos);
+			heeksCAD->Repaint();
+		}
+	}
+
+	event.Skip();
+}
+
+
 
 BEGIN_EVENT_TABLE(COutputCanvas, wxScrolledWindow)
     EVT_SIZE(COutputCanvas::OnSize)
 END_EVENT_TABLE()
 
-
-
 COutputCanvas::COutputCanvas(wxWindow* parent)
         : wxScrolledWindow(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize,
                            wxHSCROLL | wxVSCROLL | wxNO_FULL_REPAINT_ON_RESIZE)
 {
-	m_textCtrl = new wxTextCtrl( this, 100, _T(""),	wxPoint(180,170), wxSize(200,70), wxTE_MULTILINE | wxTE_READONLY | wxTE_DONTWRAP | wxTE_RICH);
+	m_textCtrl = new COutputTextCtrl( this, 100, _T(""),	wxPoint(180,170), wxSize(200,70), wxTE_MULTILINE | wxTE_DONTWRAP | wxTE_RICH);
 	Resize();
 }
 
