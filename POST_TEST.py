2dcd79f1499eb76aaa57f20094ecb612508748f6
Hirutso.Enni <Hirutso.Enni@gmail.com>
2009-01-14 21:36:43 +0000

    Fix big errors in attach

--- POST_TEST.py
+++ POST_TEST.py
@@ -1,4 +1,6 @@
 from nc.nc import *
+from nc.attach import *
+
 import nc.iso
 
 output('POST_TEST.txt')
@@ -17,3 +19,24 @@ rapid(z=50)
 rapid_home()
 
 program_end()
+
+# ATTACHED VERSION:
+
+attach_begin()
+
+program_begin(123, 'Test program, attached')
+absolute()
+metric()
+set_plane(0)
+
+feedrate(420)
+rapid(100,120)
+rapid(z=50)
+feed(z=0)
+rapid(z=50)
+
+rapid_home()
+
+program_end()
+
+attach_end()
