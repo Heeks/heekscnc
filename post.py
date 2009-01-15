df116296e90b4a25c95ad88112f8de455cc5c63e
Dan Heeks <danheeks@gmail.com>
2009-01-15 21:26:22 +0000

    fixed Linux build

--- post.py
+++ post.py
@@ -1,3 +1,17 @@
+import kurve
+import stdops
+from nc.nc import *
+from nc.attach import *
+import nc.iso
+
+output('POST_TEST.txt')
+
+program_begin(123, 'Test program')
+absolute()
+metric()
+set_plane(0)
+
+
 clearance = 5
 rapid_down_to_height = 2
 final_depth = -0.1
@@ -10,4 +24,9 @@ kurve.add_point(k1, 0, -13, -6, 0, 0)
 kurve.add_point(k1, 0, -4, 4, 0, 0)
 kurve.add_point(k1, 0, 11, 0, 0, 0)
 kurve.add_point(k1, 0, 29, 2, 0, 0)
-profile(k1, 'left', tool_diameter/2, clearance, rapid_down_to_height, final_depth)
+stdops.profile(k1, 'left', tool_diameter/2, clearance, rapid_down_to_height, final_depth)
+
+rapid_home()
+
+program_end()
+
