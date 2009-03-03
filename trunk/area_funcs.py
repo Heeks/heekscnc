37bbdc68420a347e8a2a50300692c681452c4ace
celeron55 <celeron55@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-03-03 12:14:52 +0000

    more efficient stepdown for pocketing

--- trunk/area_funcs.py
+++ trunk/area_funcs.py
@@ -2,9 +2,11 @@ import area
 from nc.nc import *
 
 def cut_area(a, do_rapid, rapid_down_to_height, final_depth):
+    print "num_curves = " + str(area.num_curves(a))
     for curve in range(0, area.num_curves(a)):
         px = 0.0
         py = 0.0
+        print "num_vertices = " + str(area.num_vertices(a, curve))
         for vertex in range(0, area.num_vertices(a, curve)):
             sp, x, y, cx, cy = area.get_vertex(a, curve, vertex)
             if do_rapid:
@@ -29,7 +31,23 @@ def cut_area(a, do_rapid, rapid_down_to_height, final_depth):
             px = x
             py = y
     
-def pocket(a, first_offset, rapid_down_to_height, start_depth, final_depth, stepover, stepdown, round_corner_factor):
+def pocket(a, first_offset, rapid_down_to_height, start_depth, final_depth, stepover, stepdown, round_corner_factor, clearance_height):
+    
+    areas = list()
+
+    offset_value = first_offset
+    a_offset = area.new()
+    area.copy(a, a_offset)
+    area.set_round_corner_factor(round_corner_factor)
+    area.offset(a_offset, offset_value)
+    
+    while area.num_curves(a_offset):
+        a_1 = area.new()
+        area.copy(a_offset, a_1)
+        areas.append(a_1)
+        area.copy(a, a_offset)
+        offset_value = offset_value + stepover
+        area.offset(a_offset, offset_value)
 
     layer_count = int((start_depth - final_depth) / stepdown)
 
@@ -50,11 +68,14 @@ def pocket(a, first_offset, rapid_down_to_height, start_depth, final_depth, step
         
         first = True
 
-        while area.num_curves(a_offset):
+        #while area.num_curves(a_offset):
+        b = 0
+        for a_offset in areas:
+            print str(b) + ':'
+            b += 1
             cut_area(a_offset, first, rapid_down_to_height, depth)
             first = False
-            area.copy(a, a_offset)
-            offset_value = offset_value + stepover
-            area.offset(a_offset, offset_value)
+
+        rapid(z = clearance_height)
 
 
