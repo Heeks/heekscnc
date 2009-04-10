dd06291fec21227669339568b796c3964f89ac55
danheeks <danheeks@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-04-10 10:56:28 +0000

    fixed operations 'inactive' http://code.google.com/p/heekscnc/issues/detail?id=76

--- src/Op.cpp
+++ src/Op.cpp
@@ -9,11 +9,13 @@
 #include "Op.h"
 #include "ProgramCanvas.h"
 #include "interface/PropertyString.h"
+#include "interface/PropertyCheck.h"
 #include "tinyxml/tinyxml.h"
 
 void COp::WriteBaseXML(TiXmlElement *element)
 {
 	if(m_comment.Len() > 0)element->SetAttribute("comment", Ttc(m_comment.c_str()));
+	if(!m_active)element->SetAttribute("active", 0);
 
 	HeeksObj::WriteBaseXML(element);
 }
@@ -22,15 +24,18 @@ void COp::ReadBaseXML(TiXmlElement* element)
 {
 	const char* comment = element->Attribute("comment");
 	if(comment)m_comment.assign(Ctt(comment));
+	const char* active = element->Attribute("active");
 
 	HeeksObj::ReadBaseXML(element);
 }
 
 static void on_set_comment(const wxChar* value, HeeksObj* object){((COp*)object)->m_comment = value;}
+static void on_set_active(bool value, HeeksObj* object){((COp*)object)->m_active = value;}
 
 void COp::GetProperties(std::list<Property *> *list)
 {
 	list->push_back(new PropertyString(_("comment"), m_comment, this, on_set_comment));
+	list->push_back(new PropertyCheck(_("active"), m_active, this, on_set_active));
 
 	HeeksObj::GetProperties(list);
 }
