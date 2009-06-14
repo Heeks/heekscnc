577e7f298c8d3a2c48ce61e433feb01e788aa51d
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-06-14 11:55:07 +0000

    Removed some 'cut-and-paste' errors that I left in comments

--- trunk/src/CuttingTool.h
+++ trunk/src/CuttingTool.h
@@ -32,37 +32,7 @@ public:
 	void ReadParametersFromXMLElement(TiXmlElement* pElem);
 };
 
-/**
-	The CCuttingTool class stores a list of symbols (by type/id pairs) of elements that represent the starting point
-	of a drilling cycle.  In the first instance, we use PointType objects as starting points.  Rather than copy
-	the PointType elements into this class, we just refer to them by ID.  In the case of PointType objects,
-	the class assumes that the drilling will occur in the negative Z direction.
-
-	One day, when I get clever, I intend supporting the reference of line elements whose length defines the
-	drill's depth and whose orientation describes the drill's orientation at machining time (i.e. rotate A, B and/or C axes)
- */
-
 class CCuttingTool: public COp {
-private:
-	/**
-		There are all types of 3d point classes around but most of them seem to be in the HeeksCAD code
-		rather than in cod that's accessible by the plugin.  I suspect I'm missing something on this
-		but, just in case I'm not, here is a special one (just for this class)
-	 */
-	typedef struct Point3d {
-		double x;
-		double y;
-		double z;
-		Point3d( double a, double b, double c ) : x(a), y(b), z(c) { }
-	} Point3d;
-
-	/**
-		The following two methods are just to draw pretty lines on the screen to represent drilling
-		cycle activity when the operator selects the CuttingTool Cycle operation in the data list.
-	 */
-	std::list< Point3d > PointsAround( const Point3d & origin, const double radius, const unsigned int numPoints ) const;
-	std::list< Point3d > DrillBitVertices( const Point3d & origin, const double radius, const double length ) const;
-
 public:
 	//	These are references to the CAD elements whose position indicate where the CuttingTool Cycle begins.
 	CCuttingToolParams m_params;
