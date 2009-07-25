3f00dbfac232468512388897c4fe300ec4a5e26c
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-07-25 00:00:25 +0000

    Almost completed adding an import and export tool to the FeedsAndSpeeds object.
    Separated the SpeedReferences (plural) out from the Program.h/cpp file into its own source files.
    
    NOTE: This version uses a modified HeeksCADInterface.  I've added the SaveXMLFile() routine to it.

--- src/Program.cpp
+++ src/Program.cpp
@@ -31,7 +31,6 @@
 #include <memory>
 using namespace std;
 
-bool CSpeedReferences::s_estimate_when_possible = true;
 
 bool COperations::CanAdd(HeeksObj* object)
 {
@@ -125,32 +124,6 @@ HeeksObj* CTools::ReadFromXMLElement(TiXmlElement* pElem)
 	return new_object;
 }
 
-void CSpeedReferences::WriteXML(TiXmlNode *root)
-{
-	TiXmlElement * element;
-	element = new TiXmlElement( "SpeedReferences" );
-	root->LinkEndChild( element );
-	element->SetAttribute("estimate_when_possible", int(CSpeedReferences::s_estimate_when_possible?1:0) );
-	WriteBaseXML(element);
-}
-
-HeeksObj* CSpeedReferences::ReadFromXMLElement(TiXmlElement* pElem)
-{
-	CSpeedReferences* new_object = new CSpeedReferences();
-	if (pElem->Attribute("estimate_when_possible"))
-	{
-		CSpeedReferences::s_estimate_when_possible = (atoi(pElem->Attribute("estimate_when_possible"))!=0);
-	} // End if - then
-	else
-	{
-		// It's not in the file.  Set to true by default.
-
-		CSpeedReferences::s_estimate_when_possible = true;
-	} // End if - else
-	new_object->ReadBaseXML(pElem);
-	return new_object;
-}
- 
 void CFixtures::WriteXML(TiXmlNode *root)
 {
 	TiXmlElement * element;
@@ -295,33 +268,6 @@ void CMachine::GetProperties(CProgram *parent, std::list<Property *> *list)
 	list->push_back(new PropertyDouble(_("Maximum Spindle Speed (RPM)"), m_max_spindle_speed, parent, on_set_max_spindle_speed));
 } // End GetProperties() method
 
-
-static void on_set_estimate_when_possible(int value, HeeksObj* object)
-{
-	CSpeedReferences::s_estimate_when_possible = (value == 0) ? true: false;
-}
-
-void CSpeedReferences::GetProperties(std::list<Property *> *list)
-{
-	{
-		std::list< wxString > choices;
-		int choice = 0;
-		choices.push_back( _T("Estimate when possible") );		// true
-		choices.push_back( _T("Use default values") );		// false
-
-		if (CSpeedReferences::s_estimate_when_possible) 
-		{
-			choice = 0;
-		} // End if - then
-		else
-		{
-			choice = 1;
-		} // End if - else
-
-		list->push_back ( new PropertyChoice ( _("Feeds and Speeds"),  choices, choice, this, on_set_estimate_when_possible ) );
-	}
-	HeeksObj::GetProperties(list);
-}
 
 
 bool CProgram::CanAdd(HeeksObj* object)
@@ -883,99 +829,6 @@ CMachine CProgram::GetMachine(const wxString& file_name)
 	machine.description = _T("not found");
 	return machine;
 }
-
-/**
-	This method finds a distinct set of material names from the SpeedReferences list.
- */
-std::set< wxString > CSpeedReferences::GetMaterials()
-{
-	std::set< wxString > materials;
-	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
-	{
-		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
-			material != NULL;
-			material = theApp.m_program->m_speed_references->GetNextChild())
-		{
-			if (material->GetType() != SpeedReferenceType) continue;
-
-			materials.insert( ((CSpeedReference *)material)->m_material_name );
-		} // End for
-	} // End if - then
-
-	return(materials);
-} // End GetMaterials() method
-
-/**
-	Return a set of harndess values that are valid for the chosen material name.
- */
-std::set< double > CSpeedReferences::GetHardnessForMaterial( const wxString & material_name )
-{
-	std::set< double > hardness_values;
-	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
-	{
-		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
-			material != NULL;
-			material = theApp.m_program->m_speed_references->GetNextChild())
-		{
-			if (material->GetType() != SpeedReferenceType) continue;
-
-			if (material_name == ((CSpeedReference *) material)->m_material_name)
-			{
-				hardness_values.insert( ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material );
-			} // End if - then
-		} // End for
-	} // End if - then
-
-	return(hardness_values);
-
-} // End of GetHardnessForMaterial() method
-
-/**
-	Return a set of harndess values that have already been configured.
- */
-std::set< double > CSpeedReferences::GetAllHardnessValues()
-{
-	std::set< double > hardness_values;
-	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
-	{
-		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
-			material != NULL;
-			material = theApp.m_program->m_speed_references->GetNextChild())
-		{
-			if (material->GetType() != SpeedReferenceType) continue;
-			hardness_values.insert( ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material );
-		} // End for
-	} // End if - then
-
-	return(hardness_values);
-
-} // End of GetAllHardnessValues() method
-
-double CSpeedReferences::GetSurfaceSpeed( 
-	const wxString & material_name, 
-	const int cutting_tool_material, 
-	const double brinell_hardness_of_raw_material )
-{
-	if (theApp.m_program == NULL) return(-1.0);
-	if (theApp.m_program->m_speed_references == NULL) return(-1.0);
-
-	for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
-		material != NULL;
-		material = theApp.m_program->m_speed_references->GetNextChild())
-	{
-		if (material->GetType() != SpeedReferenceType) continue;
-		if ((material_name == ((CSpeedReference *) material)->m_material_name) &&
-		    (cutting_tool_material == ((CSpeedReference *) material)->m_cutting_tool_material) &&
-		    (brinell_hardness_of_raw_material == ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material))
-		{
-			return( ((CSpeedReference *) material)->m_surface_speed );
-		} // End if - then
-	} // End for
-
-	// The data doesn't exist for this combination of inputs.
-	return(-1.0);
-	
-} // End GetSurfaceSpeed() method
 
 
 /**
---
+++ src/SpeedReferences.cpp
@@ -0,0 +1,212 @@
+// SpeedReferences.cpp
+
+#include "stdafx.h"
+#include "tinyxml/tinyxml.h"
+#include "ProgramCanvas.h"
+#include "interface/PropertyString.h"
+#include "interface/PropertyChoice.h"
+#include "interface/PropertyDouble.h"
+#include "interface/PropertyLength.h"
+#include "interface/Tool.h"
+#include "CuttingTool.h"
+#include "Op.h"
+#include "CNCConfig.h"
+#include "SpeedOp.h"
+#include "interface/strconv.h"
+
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <memory>
+using namespace std;
+
+bool CSpeedReferences::s_estimate_when_possible = true;
+
+class ExportSpeedReferences: public Tool{
+	// Tool's virtual functions
+	const wxChar* GetTitle(){return _("Export");}
+	void Run()
+	{
+		std::list<HeeksObj *> speed_references;
+		for (HeeksObj *speed_reference = theApp.m_program->m_speed_references->GetFirstChild();
+			speed_reference != NULL;
+			speed_reference = theApp.m_program->m_speed_references->GetNextChild() )
+		{
+			speed_references.push_back( speed_reference );
+		} // End for
+
+		heeksCAD->SaveXMLFile( speed_references, _T("default.speeds"), false );
+	}
+	wxString BitmapPath(){ return _T("export");}
+};
+
+static ExportSpeedReferences export_speed_references;
+
+class ImportSpeedReferences: public Tool{
+	// Tool's virtual functions
+	const wxChar* GetTitle(){return _("Import");}
+	void Run()
+	{
+		heeksCAD->OpenXMLFile( _T("default.speeds"), true, theApp.m_program->m_speed_references );
+	}
+	wxString BitmapPath(){ return _T("import");}
+};
+
+static ImportSpeedReferences import_speed_references;
+
+void CSpeedReferences::GetTools(std::list<Tool*>* t_list, const wxPoint* p)
+{
+	// t_list->push_back(&import_speed_references);
+	// t_list->push_back(&export_speed_references);
+
+	ObjList::GetTools(t_list, p);
+}
+
+void CSpeedReferences::WriteXML(TiXmlNode *root)
+{
+	TiXmlElement * element;
+	element = new TiXmlElement( "SpeedReferences" );
+	root->LinkEndChild( element );
+	element->SetAttribute("estimate_when_possible", int(CSpeedReferences::s_estimate_when_possible?1:0) );
+	WriteBaseXML(element);
+}
+
+HeeksObj* CSpeedReferences::ReadFromXMLElement(TiXmlElement* pElem)
+{
+	CSpeedReferences* new_object = new CSpeedReferences();
+	if (pElem->Attribute("estimate_when_possible"))
+	{
+		CSpeedReferences::s_estimate_when_possible = (atoi(pElem->Attribute("estimate_when_possible"))!=0);
+	} // End if - then
+	else
+	{
+		// It's not in the file.  Set to true by default.
+
+		CSpeedReferences::s_estimate_when_possible = true;
+	} // End if - else
+	new_object->ReadBaseXML(pElem);
+	return new_object;
+}
+ 
+static void on_set_estimate_when_possible(int value, HeeksObj* object)
+{
+	CSpeedReferences::s_estimate_when_possible = (value == 0) ? true: false;
+}
+
+void CSpeedReferences::GetProperties(std::list<Property *> *list)
+{
+	{
+		std::list< wxString > choices;
+		int choice = 0;
+		choices.push_back( _T("Estimate when possible") );		// true
+		choices.push_back( _T("Use default values") );		// false
+
+		if (CSpeedReferences::s_estimate_when_possible) 
+		{
+			choice = 0;
+		} // End if - then
+		else
+		{
+			choice = 1;
+		} // End if - else
+
+		list->push_back ( new PropertyChoice ( _("Feeds and Speeds"),  choices, choice, this, on_set_estimate_when_possible ) );
+	}
+	HeeksObj::GetProperties(list);
+}
+
+
+/**
+	This method finds a distinct set of material names from the SpeedReferences list.
+ */
+std::set< wxString > CSpeedReferences::GetMaterials()
+{
+	std::set< wxString > materials;
+	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
+	{
+		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
+			material != NULL;
+			material = theApp.m_program->m_speed_references->GetNextChild())
+		{
+			if (material->GetType() != SpeedReferenceType) continue;
+
+			materials.insert( ((CSpeedReference *)material)->m_material_name );
+		} // End for
+	} // End if - then
+
+	return(materials);
+} // End GetMaterials() method
+
+/**
+	Return a set of harndess values that are valid for the chosen material name.
+ */
+std::set< double > CSpeedReferences::GetHardnessForMaterial( const wxString & material_name )
+{
+	std::set< double > hardness_values;
+	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
+	{
+		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
+			material != NULL;
+			material = theApp.m_program->m_speed_references->GetNextChild())
+		{
+			if (material->GetType() != SpeedReferenceType) continue;
+
+			if (material_name == ((CSpeedReference *) material)->m_material_name)
+			{
+				hardness_values.insert( ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material );
+			} // End if - then
+		} // End for
+	} // End if - then
+
+	return(hardness_values);
+
+} // End of GetHardnessForMaterial() method
+
+/**
+	Return a set of harndess values that have already been configured.
+ */
+std::set< double > CSpeedReferences::GetAllHardnessValues()
+{
+	std::set< double > hardness_values;
+	if ((theApp.m_program) && (theApp.m_program->m_speed_references))
+	{
+		for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
+			material != NULL;
+			material = theApp.m_program->m_speed_references->GetNextChild())
+		{
+			if (material->GetType() != SpeedReferenceType) continue;
+			hardness_values.insert( ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material );
+		} // End for
+	} // End if - then
+
+	return(hardness_values);
+
+} // End of GetAllHardnessValues() method
+
+double CSpeedReferences::GetSurfaceSpeed( 
+	const wxString & material_name, 
+	const int cutting_tool_material, 
+	const double brinell_hardness_of_raw_material )
+{
+	if (theApp.m_program == NULL) return(-1.0);
+	if (theApp.m_program->m_speed_references == NULL) return(-1.0);
+
+	for (HeeksObj *material = theApp.m_program->m_speed_references->GetFirstChild();
+		material != NULL;
+		material = theApp.m_program->m_speed_references->GetNextChild())
+	{
+		if (material->GetType() != SpeedReferenceType) continue;
+		if ((material_name == ((CSpeedReference *) material)->m_material_name) &&
+		    (cutting_tool_material == ((CSpeedReference *) material)->m_cutting_tool_material) &&
+		    (brinell_hardness_of_raw_material == ((CSpeedReference *) material)->m_brinell_hardness_of_raw_material))
+		{
+			return( ((CSpeedReference *) material)->m_surface_speed );
+		} // End if - then
+	} // End for
+
+	// The data doesn't exist for this combination of inputs.
+	return(-1.0);
+	
+} // End GetSurfaceSpeed() method
+
+
