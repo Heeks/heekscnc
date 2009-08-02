6f5ff012a25434b59e9215e3b636d9e164dcc56d
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-08-02 12:57:36 +0000

    Fixed bug.  I added the LocatingType to the CProgram::CanAdd() method where it should only have been found in the COp::IsAnOperation() method.

--- trunk/src/Program.cpp
+++ trunk/src/Program.cpp
@@ -276,7 +276,6 @@ bool CProgram::CanAdd(HeeksObj* object)
 		object->GetType() == OperationsType || 
 		object->GetType() == ToolsType || 
 		object->GetType() == SpeedReferencesType ||
-		object->GetType() == LocatingType ||
 		object->GetType() == FixturesType;
 }
 
