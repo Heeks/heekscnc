1ee6c7412184bd5008dfe5d71d0daebff77e4ff7
jonpry <jonpry@gmail.com>
2009-10-08 20:11:13 +0000

    Added ModifyByMatrix to CTrsfNCCode so that the programs can be moved around manually before/after/instead of automatic packing.

--- src/TrsfNCCode.h
+++ src/TrsfNCCode.h
@@ -21,6 +21,7 @@ public:
 
 	// HeeksObj's virtual functions
 	int GetType()const{return TrsfNCCodeType;}
+	long GetMarkingMask()const{return MARKING_FILTER_UNKNOWN;}
 	const wxChar* GetTypeString(void)const{return _T("Transformable NC Code");}
 	wxString GetIcon(){return theApp.GetResFolder() + _T("/icons/program");}
 	HeeksObj *MakeACopy(void)const;
@@ -30,4 +31,5 @@ public:
 	bool OneOfAKind(){return true;}
 	static HeeksObj* ReadFromXMLElement(TiXmlElement* pElem);
 	void glCommands(bool select, bool marked, bool no_color);
+	bool ModifyByMatrix(const double *m);
 };
