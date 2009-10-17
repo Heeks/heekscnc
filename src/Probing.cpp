c1d1781cd14b51353e036f69daba75b92427f8b4
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2009-10-17 23:29:35 +0000

    Got first version of Probe_LinearCentre_Outside class working right through to the machine.

--- src/Probing.cpp
+++ src/Probing.cpp
@@ -15,6 +15,7 @@
 #include "interface/PropertyLength.h"
 #include "interface/PropertyChoice.h"
 #include "interface/Tool.h"
+#include "interface/strconv.h"
 #include "tinyxml/tinyxml.h"
 #include "Operations.h"
 #include "CuttingTool.h"
@@ -46,7 +47,7 @@ static void on_set_starting_distance(double value, HeeksObj* object)
 
 static void on_set_depth(double value, HeeksObj* object)
 {
-	((CProbe_LinearCentre_Outside*)object)->m_depth = value;
+	((CProbing*)object)->m_depth = value;
 }
 
 void CProbe_LinearCentre_Outside::AppendTextToProgram( const CFixture *pFixture )
@@ -71,38 +72,39 @@ void CProbe_LinearCentre_Outside::AppendTextToProgram( const CFixture *pFixture 
 	ss << "comment('before probing back towards the centre point looking for the')\n";
 	ss << "comment('protrusion.')\n";
 
+	gp_Dir z_direction(0,0,1);
+
 	CNCPoint first(m_starting_distance,0,0);
-	CNCPoint second(m_starting_distance,0,0);
-
-	gp_Dir x_direction(1,0,0);
-
-	double first_angle_in_radians = (m_starting_angle / 360) * (2 * PI);	// degrees expressed in radians
-	while (first_angle_in_radians < 0) first_angle_in_radians += (2 * PI);
+	double angle_in_radians = (m_starting_angle / 360) * (2 * PI);	// degrees expressed in radians
+	while (angle_in_radians < 0) angle_in_radians += (2 * PI);
 	gp_Trsf first_rotation_matrix;
-	first_rotation_matrix.SetRotation( gp_Ax1( gp_Pnt(0,0,0), x_direction), first_angle_in_radians );
+	first_rotation_matrix.SetRotation( gp_Ax1( gp_Pnt(0,0,0), z_direction), angle_in_radians );
 	first.Transform(first_rotation_matrix);
 
-	double second_angle_in_radians = first_angle_in_radians + PI;	// half a circle around.
-	while (second_angle_in_radians < 0) second_angle_in_radians += (2 * PI);
+	CNCPoint second(-1.0 * m_starting_distance,0,0);
 	gp_Trsf second_rotation_matrix;
-	second_rotation_matrix.SetRotation( gp_Ax1( gp_Pnt(0,0,0), x_direction), second_angle_in_radians );
+	second_rotation_matrix.SetRotation( gp_Ax1( gp_Pnt(0,0,0), z_direction), angle_in_radians );
 	second.Transform(second_rotation_matrix);
+
+	// Make sure it's negative as we're stepping down.  There is no option
+	// to specify a starting height so this can only be a relative distance.
+	double relative_depth = (m_depth > 0)?(-1.0 * m_depth):m_depth;
 
 	ss << "probe_linear_centre_outside("
 		<< "x1=" << first.X(true) << ", "
 		<< "y1=" << first.Y(true) << ", "
-		<< "depth=" << m_depth / theApp.m_program->m_units << ", "
+		<< "depth=" << relative_depth / theApp.m_program->m_units << ", "
 		<< "x2=" << second.X(true) << ", "
 		<< "y2=" << second.Y(true) << ", "
-		<< "xml_file_name='" << this->GetOutputFileName(wxString(_(".xml"))).c_str() << "'"
+		<< "xml_file_name='" << this->GetOutputFileName(wxString(_(".xml")), true).c_str() << "'"
 		<< ")\n";
 
 	theApp.m_program_canvas->m_textCtrl->AppendText(ss.str().c_str());
 }
 
-
 void CProbing::GetProperties(std::list<Property *> *list)
 {
+	list->push_back(new PropertyLength(_("Depth"), m_depth, this, on_set_depth));
 	CSpeedOp::GetProperties(list);
 }
 
@@ -110,7 +112,6 @@ void CProbe_LinearCentre_Outside::GetProperties(std::list<Property *> *list)
 {
 	list->push_back(new PropertyDouble(_("Starting angle"), m_starting_angle, this, on_set_starting_angle));
 	list->push_back(new PropertyLength(_("Starting distance"), m_starting_distance, this, on_set_starting_distance));
-	list->push_back(new PropertyLength(_("Depth"), m_depth, this, on_set_depth));
 	CProbing::GetProperties(list);
 }
 
@@ -136,7 +137,6 @@ void CProbe_LinearCentre_Outside::WriteXML(TiXmlNode *root)
 
 	element->SetDoubleAttribute("starting_angle", m_starting_angle);
 	element->SetDoubleAttribute("starting_distance", m_starting_distance);
-	element->SetDoubleAttribute("depth", m_depth);
 
 	WriteBaseXML(element);
 }
@@ -146,24 +146,33 @@ HeeksObj* CProbe_LinearCentre_Outside::ReadFromXMLElement(TiXmlElement* element)
 {
 	CProbe_LinearCentre_Outside* new_object = new CProbe_LinearCentre_Outside();
 
-	for(TiXmlElement* pElem = TiXmlHandle(element).FirstChildElement().Element(); pElem; pElem = pElem->NextSiblingElement())
-	{
-		if (pElem->Attribute("starting_angle")) new_object->m_starting_angle = atof(pElem->Attribute("starting_angle"));
-		if (pElem->Attribute("starting_distance")) new_object->m_starting_distance = atof(pElem->Attribute("starting_distance"));
-		if (pElem->Attribute("depth")) new_object->m_depth = atof(pElem->Attribute("depth"));
-	}
+	if (element->Attribute("starting_angle")) new_object->m_starting_angle = atof(element->Attribute("starting_angle"));
+	if (element->Attribute("starting_distance")) new_object->m_starting_distance = atof(element->Attribute("starting_distance"));
 
 	new_object->ReadBaseXML(element);
 
 	return new_object;
 }
 
+void CProbing::WriteBaseXML(TiXmlElement *element)
+{
+	element->SetDoubleAttribute("depth", m_depth);
+	CSpeedOp::WriteBaseXML(element);
+}
+
+void CProbing::ReadBaseXML(TiXmlElement* element)
+{
+	if (element->Attribute("depth")) m_depth = atof(element->Attribute("depth"));
+	CSpeedOp::ReadBaseXML(element);
+}
+
+
 void CProbe_LinearCentre_Outside::glCommands(bool select, bool marked, bool no_color)
 {
 }
 
 
-wxString CProbe_LinearCentre_Outside::GetOutputFileName(const wxString extension)
+wxString CProbe_LinearCentre_Outside::GetOutputFileName(const wxString extension, const bool filename_only)
 {
 
 	wxString file_name = theApp.m_program->GetOutputFileName();
@@ -175,9 +184,20 @@ wxString CProbe_LinearCentre_Outside::GetOutputFileName(const wxString extension
 		file_name.Remove(offset);
 	}
 
-	file_name << _("_Probe_Linear_Centre_Outside_id_");
+	file_name << _("_");
+	file_name << m_title;
+	file_name << _("_id_");
 	file_name << m_id;
 	file_name << extension;
+
+	if (filename_only)
+	{
+		std::vector<wxString> tokens = Tokens( file_name, wxString(_("/\\")) );
+		if (tokens.size() > 0)
+		{
+			file_name = tokens[tokens.size()-1];
+		}
+	}
 
 	return(file_name);
 }
@@ -224,7 +244,7 @@ public:
 		} // End if - else
 
 		// output file
-		theApp.m_program_canvas->AppendText(_T("output('") + m_pThis->GetOutputFileName(_(".tap")) + _T("')\n"));
+		theApp.m_program_canvas->AppendText(_T("output('") + m_pThis->GetOutputFileName(_(".tap"), false) + _T("')\n"));
 
 		// begin program
 		theApp.m_program_canvas->AppendText(_T("program_begin(123, 'Test program')\n"));
@@ -248,11 +268,11 @@ public:
 
 		{
 			// clear the output file
-			wxFile f(m_pThis->GetOutputFileName(_(".tap")).c_str(), wxFile::write);
+			wxFile f(m_pThis->GetOutputFileName(_(".tap"), false).c_str(), wxFile::write);
 			if(f.IsOpened())f.Write(_T("\n"));
 		}
 
-		HeeksPyPostProcess(theApp.m_program, m_pThis->GetOutputFileName(_(".tap")), false );
+		HeeksPyPostProcess(theApp.m_program, m_pThis->GetOutputFileName(_(".tap"), false), false );
 	}
 	wxString BitmapPath(){ return _T("export");}
 	wxString previous_path;
