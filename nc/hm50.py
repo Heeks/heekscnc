26f1602211ba09d560790a2a45b176f8e2286380
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2010-03-09 23:48:57 +0000

    Changed the tool_change() routine to move the spindle up to a safe height between tool changes.  This is not my long-term solution but it will do for now.

--- nc/hm50.py
+++ nc/hm50.py
@@ -1,11 +1,21 @@
 import nc
+import iso_codes
 import emc2
 
+iso_codes.codes = emc2.CodesEMC2()
+
 class CreatorHM50(emc2.CreatorEMC2):
-    def init(self): 
-        iso.CreatorEMC2.init(self) 
+	def init(self): 
+		iso.CreatorEMC2.init(self) 
 	
-    def program_begin(self, id, comment):
-	self.write( ('(' + comment + ')' + '\n') )
+	def program_begin(self, id, comment):
+		self.write( ('(' + comment + ')' + '\n') )
+
+	def tool_change(self, id):
+		self.write_blocknum()
+		self.write('G53 G00 Z350\n')
+		self.write_blocknum()
+		self.write((iso_codes.codes.TOOL() % id) + '\n')
+
 
 nc.creator = CreatorHM50()
