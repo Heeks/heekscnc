7bd96af782de617b68565f488a9363aca017a6d1
Dan Heeks <danheeks@gmail.com>
2010-05-04 10:19:49 +0000

    (Forgot to add these two files ) I made a machine with modal F and modal G0, G1, G2, G3

---
+++ nc/iso_modal.py
@@ -0,0 +1,23 @@
+################################################################################
+# iso_modal.py
+#
+# a class derived from iso machine, but with XYZF G1, G2 etc modal to reduce the size of the file.
+# It is just an ISO machine, but I don't want the tool definition lines
+#
+# Dan Heeks, 4th May 2010
+
+import nc
+import iso
+import math
+
+################################################################################
+class CreatorIsoModal(iso.CreatorIso):
+
+    def __init__(self):
+        iso.CreatorIso.__init__(self)
+        self.f_modal = True
+        self.g0123_modal = True
+
+################################################################################
+
+nc.creator = CreatorIsoModal()
