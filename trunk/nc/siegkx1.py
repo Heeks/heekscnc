d78c787e41356ca8f3c4706b47431b0cfac1f580
danheeks <danheeks@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2010-05-04 10:17:16 +0000

    I made a machine with modal F and modal G0, G1, G2, G3

--- trunk/nc/siegkx1.py
+++ trunk/nc/siegkx1.py
@@ -1,19 +1,20 @@
 ################################################################################
-# iso.py
+# siegkx1.py
 #
-# Simple ISO NC code creator
+# Post Processor for the Sieg KX1 machine
+# It is just an ISO machine, but I don't want the tool definition lines
 #
 # Dan Heeks, 5th March 2009
 
 import nc
-import iso
+import iso_modal
 import math
 
 ################################################################################
-class CreatorSieg(iso.CreatorIso):
+class CreatorSieg(iso_modal.CreatorIsoModal):
 
     def __init__(self):
-        iso.CreatorIso.__init__(self)
+        iso_modal.CreatorIsoModal.__init__(self)
 
     def tool_defn(self, id, name='', radius=None, length=None):
         pass
