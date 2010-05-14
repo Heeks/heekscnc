6198a674b77f4419f0467dec547e3ca7798d9d4b
David.Nicholls.homeip.net <David.Nicholls.homeip.net@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2010-05-14 07:48:03 +0000

    Fixed problem in edge probing operation.
    Added option to have two private fixtures added to the Inlay operation so that the male and female halves could be machined in separate fixtures.
    
    Added 'touch-off point' and 'touch-off description' fields to the Fixture objects.  This allows an X,Y rapid movement when moving between two fixtures.  It also allows a comment to be added to the HeeksCNC model indicating how the fixture should be setup.  eg: 'set 0,0 point to bottom left corner'.  This is added as a comment in the resultant GCode.

--- src/MachineState.cpp
+++ src/MachineState.cpp
@@ -80,6 +80,10 @@ Python CMachineState::CuttingTool( const int new_cutting_tool )
     next fixture.  If it is, indeed, a different fixture then issue the appropriate GCode to make the switch.  This
     routine should not add the GCode unless it's necessary.  We want the AppendTextToProgram() methods to be
     able to call this routine repeatedly without worrying about unnecessary movements.
+
+	If we're moving between two different fixtures, move above the new fixture's touch-off point before
+	continuing on with the other machine operations.  This ensures that the cutting tool is somewhere above
+	the new fixture before we start any other movements.
  */
 Python CMachineState::Fixture( CFixture new_fixture )
 {
@@ -87,13 +91,15 @@ Python CMachineState::Fixture( CFixture new_fixture )
 
     if ((m_fixture != new_fixture) || (! m_fixture_has_been_set))
     {
-        m_fixture_has_been_set = true;
-
         // The fixture has been changed.  Move to the highest safety-height between the two fixtures.
         if (m_fixture.m_params.m_safety_height_defined)
         {
 			if (new_fixture.m_params.m_safety_height_defined)
 			{
+				wxString comment;
+				comment << _("Moving to a safety height common to both ") << m_fixture.m_coordinate_system_number << _(" and ") << new_fixture.m_coordinate_system_number;
+				python << _T("comment(") << PythonString(comment) << _T(")\n");
+
 				// Both fixtures have a safety height defined.  Move the highest of the two.
 				if (m_fixture.m_params.m_safety_height > new_fixture.m_params.m_safety_height)
 				{
@@ -111,7 +117,26 @@ Python CMachineState::Fixture( CFixture new_fixture )
 			} // End if - else            
         }
 
+		// Invoke new coordinate system.
 		python << new_fixture.AppendTextToProgram();
+
+		if (m_fixture_has_been_set == true)
+		{
+			// We must be moving between fixtures rather than an initial fixture setup.
+
+			// Now move to above the touch-off point so that, when we plunge down, we won't hit the old fixture.
+			if (new_fixture.m_params.m_touch_off_description.Length() > 0)
+			{
+				python << _T("comment(") << PythonString(new_fixture.m_params.m_touch_off_description) << _T(")\n");
+			}
+
+			wxString comment;
+			comment << _("Move above touch-off point for ") << new_fixture.m_coordinate_system_number;
+			python << _T("comment(") << PythonString(comment) << _T(")\n");
+			python << _T("rapid(x=") << new_fixture.m_params.m_touch_off_point.X() << _T(", y=") << new_fixture.m_params.m_touch_off_point.Y() << _T(")\n");
+		} // End if - then
+
+		m_fixture_has_been_set = true;
     }
 
     m_fixture = new_fixture;
