8f5776bcd9fedc002c6ecf9957bbb9d0f02ffc6f
ddfalck2002@yahoo.com <ddfalck2002@yahoo.com@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2010-05-16 23:14:14 +0000

    removing def drill from centroid1.py. you'll see why in a bit...

--- trunk/nc/centroid1.py
+++ trunk/nc/centroid1.py
@@ -40,14 +40,45 @@ class CreatorCentroid1(iso_modal.CreatorIsoModal):
 
     def program_end(self):
         self.write('M05\n')
-        self.write('M25\n')
+        self.write('G49 M25\n')
         self.write('G00 X-1.0 Y1.0\n')
         self.write('G17 G80 G40 G90\n')
         self.write('M99\n')
+
+    def program_stop(self, optional=False):
+        self.write_blocknum()
+        if (optional) : 
+            self.write(iso.codes.STOP_OPTIONAL() + '\n')
+        else : 
+            self.write('M05\n')
+            self.write('G49 M25\n')
+            self.write(iso.codes.STOP() + '\n')
+            self.prev_g0123 = ''
+################################################################################
+# coordinate system ie G54-G59
+
+    def workplane(self, id):
+        self.write('M25\n')
+        if ((id >= 1) and (id <= 6)):
+            self.g += iso.codes.WORKPLANE() % (id + iso.codes.WORKPLANE_BASE())
+        if ((id >= 7) and (id <= 9)):
+            self.g += ((iso.codes.WORKPLANE() % (6 + iso.codes.WORKPLANE_BASE())) + ('.%i' % (id - 6)))
+        self.prev_g0123 = ''            
+
+
                      
 ################################################################################
 # tool info
+    def tool_change(self, id):
+        self.write_blocknum()
+        self.write((iso.codes.TOOL() % id) + '\n')
+        self.t = id
+        self.write('G49 M25\n')
+        self.write('G43 H'+ str(id) + 'Z.75 \n')
+
+
     def tool_defn(self, id, name='', radius=None, length=None):
+        #self.write('G43 \n')
         pass
 
     def write_spindle(self):
@@ -55,131 +86,22 @@ class CreatorCentroid1(iso_modal.CreatorIsoModal):
 
 
     def spindle(self, s, clockwise):
-	if s < 0: clockwise = not clockwise
-	s = abs(s)
+        if s < 0: 
+            clockwise = not clockwise
+            s = abs(s)
+        
         self.s = iso.codes.SPINDLE(iso.codes.FORMAT_ANG(), s)
-	if clockwise:
-                #self.s =  iso.codes.SPINDLE_CW() + self.s
-		self.s =  iso.codes.SPINDLE_CW()                
-                self.write(self.s +  '\n')
-                self.write('G04 P2.0 \n')
-	else:
-		self.s =  iso.codes.SPINDLE_CCW() + self.s
-
-################################################################################
-# feedrate
-
-    def imperial(self):
-        self.g += iso.codes.IMPERIAL()
-        self.fmt = iso.codes.FORMAT_IN()
-        self.ffmt = iso.codes.FORMAT_FEEDRATE() 
-
-    def feedrate(self, f):
-               
-        self.f = iso.codes.FEEDRATE() + (self.ffmt % f)
-        self.fhv = False
-
-    def feedrate_hv(self, fh, fv):
-        self.fh = fh
-        self.fv = fv
-        self.fhv = True
-
-    def calc_feedrate_hv(self, h, v):
-        if math.fabs(v) > math.fabs(h * 2):
-            # some horizontal, so it should be fine to use the horizontal feed rate
-            self.f = iso.codes.FEEDRATE() + (self.ffmt % self.fv)
+        if clockwise:
+           #self.s =  iso.codes.SPINDLE_CW() + self.s
+            self.s =  iso.codes.SPINDLE_CW()                
+            self.write(self.s +  '\n')
+            self.write('G04 P2.0 \n')
+                
         else:
-            # not much, if any horizontal component, so use the vertical feed rate
-            self.f = iso.codes.FEEDRATE() + (self.ffmt % self.fh)
+            self.s =  iso.codes.SPINDLE_CCW() + self.s
 
-################################################################################
-# drill cycle
 
-    def drill(self, x=None, y=None, z=None, depth=None, standoff=None, dwell=None, peck_depth=None):
-        if (standoff == None):        
-        # This is a bad thing.  All the drilling cycles need a retraction (and starting) height.        
-            return
-           
-        if (z == None): 
-            return	# We need a Z value as well.  This input parameter represents the top of the hole    	          
-        self.write_preps()
-        self.write_blocknum()                
-        
-        if (peck_depth != 0):        
-            # We're pecking.  Let's find a tree. 
-                if self.drill_modal:       
-                    if  iso.codes.PECK_DRILL() + iso.codes.PECK_DEPTH(self.ffmt, peck_depth) != self.prev_drill:
-                        self.write(iso.codes.PECK_DRILL() + iso.codes.PECK_DEPTH(self.ffmt, peck_depth))  
-                        self.prev_drill = iso.codes.PECK_DRILL() + iso.codes.PECK_DEPTH(self.ffmt, peck_depth)
-                else:       
-                    self.write(iso.codes.PECK_DRILL() + iso.codes.PECK_DEPTH(self.ffmt, peck_depth)) 
-                           
-        else:        
-            # We're either just drilling or drilling with dwell.        
-            if (dwell == 0):        
-                # We're just drilling. 
-                if self.drill_modal:       
-                    if  iso.codes.DRILL() != self.prev_drill:
-                        self.write(iso.codes.DRILL())  
-                        self.prev_drill = iso.codes.DRILL()
-                else:
-                    self.write(iso.codes.DRILL())
-      
-            else:        
-                # We're drilling with dwell.
 
-                if self.drill_modal:       
-                    if  iso.codes.DRILL_WITH_DWELL(iso.codes.FORMAT_DWELL(),dwell) != self.prev_drill:
-                        self.write(iso.codes.DRILL_WITH_DWELL(iso.codes.FORMAT_DWELL(),dwell))  
-                        self.prev_drill = iso.codes.DRILL_WITH_DWELL(iso.codes.FORMAT_DWELL(),dwell)
-                else:
-                    self.write(iso.codes.DRILL_WITH_DWELL(iso.codes.FORMAT_DWELL(),dwell))
-
-        
-                #self.write(iso.codes.DRILL_WITH_DWELL(iso.codes.FORMAT_DWELL(),dwell))                
-    
-    # Set the retraction point to the 'standoff' distance above the starting z height.        
-        retract_height = z + standoff        
-        if (x != None):        
-            dx = x - self.x        
-            self.write(iso.codes.X() + (self.ffmt % x))        
-            self.x = x 
-       
-        if (y != None):        
-            dy = y - self.y        
-            self.write(iso.codes.Y() + (self.ffmt % y))        
-            self.y = y
-                      
-        dz = (z + standoff) - self.z # In the end, we will be standoff distance above the z value passed in.
-
-        if self.drill_modal:
-            if z != self.prev_z:
-                self.write(iso.codes.Z() + (self.ffmt % (z - depth)))
-                self.prev_z=z
-        else: 			
-	    self.write(iso.codes.Z() + (self.ffmt % (z - depth)))	# This is the 'z' value for the bottom of the hole.
-	self.z = (z + standoff)			# We want to remember where z is at the end (at the top of the hole)
-
-        if self.drill_modal:
-            if iso.codes.RETRACT(self.ffmt, retract_height) != self.prev_retract:
-                self.write(iso.codes.RETRACT(self.ffmt, retract_height))               
-                self.prev_retract = iso.codes.RETRACT(self.ffmt, retract_height)
-        else:              
-            self.write(iso.codes.RETRACT(self.ffmt, retract_height)) 
-           
-        if (self.fhv) : 
-            self.calc_feedrate_hv(math.sqrt(dx*dx+dy*dy), math.fabs(dz))
-
-        if self.drill_modal:
-            if ( iso.codes.FEEDRATE() + (self.ffmt % self.fv) + iso.codes.SPACE() )!= self.prev_f:
-               self.write(iso.codes.FEEDRATE() + (self.ffmt % self.fv) + iso.codes.SPACE() )        
-               self.prev_f = iso.codes.FEEDRATE() + (self.ffmt % self.fv) + iso.codes.SPACE()
-        else: 
-            self.write( iso.codes.FEEDRATE() + (self.ffmt % self.fv) + iso.codes.SPACE() )            
-        self.write_spindle()            
-        #self.write_misc()    
-        self.write('\n')
-        
 
 
 
