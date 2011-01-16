c295054fc1e1e1559aad013596841b27451db988
Dan Heeks <danheeks@gmail.com>
2011-01-16 18:18:18 +0000

    I got NoCad and HeeksCAD versions creating NC code OK for a program with a pocket operation.

--- Pocket.py
+++ Pocket.py
@@ -3,8 +3,6 @@ import HeeksCNC
 from consts import *
 import wx
 from CNCConfig import CNCConfig
-import re
-pattern_main = re.compile('([(!;].*|\s+|[a-zA-Z0-9_:](?:[+-])?\d*(?:\.\d*)?|\w\#\d+|\(.*?\)|\#\d+\=(?:[+-])?\d*(?:\.\d*)?)')
 
 class Pocket(DepthOp):
     def __init__(self):
@@ -61,13 +59,8 @@ class Pocket(DepthOp):
             return # to do, area_funcs.pocket crashes if given an empty area
         
         DepthOp.AppendTextToProgram(self)
-        HeeksCNC.program.python_program += "a = area.Area()\n"
+        HeeksCNC.cad.WriteAreaToProgram(self.sketches)
         HeeksCNC.program.python_program += "entry_moves = []\n"
-
-        for sketch in self.sketches:
-            shape_str = HeeksCNC.cad.GetSketchShape(sketch)
-            if shape_str:
-                WriteAreaCurve(shape_str)
 
         # reorder the area, the outside curves must be made anti-clockwise and the insides clockwise
         HeeksCNC.program.python_program += "a.Reorder()\n"
@@ -92,52 +85,3 @@ class Pocket(DepthOp):
 
         # rapid back up to clearance plane
         HeeksCNC.program.python_program += "rapid(z = clearance)\n"
-
-def WriteLine(words):
-    if words[0][0] != "x":return
-    x = words[0][1:]
-    if words[1][0] != "y":return
-    y = words[1][1:]
-    HeeksCNC.program.python_program += "c.append(area.Point(" + x + ", " + y + "))\n"
-
-def WriteArc(direction, words):
-    type_str = "-1"
-    if direction: type_str = "1"
-    if words[1][0] != "x":return
-    x = words[1][1:]
-    if words[2][0] != "y":return
-    y = words[2][1:]
-    if words[3][0] != "x":return
-    x = words[3][1:]
-    if words[4][0] != "y":return
-    y = words[4][1:]
-    HeeksCNC.program.python_program += "c.append(area.Vertex(" + type_str + ", area.Point(" + x + ", " + y + "), area.Point(" + i + ", " + j + ")))\n"
-
-def WriteSpan(span_str):
-    global pattern_main
-    words = pattern_main.findall(span_str)
-    length = len(words)
-    if length < 1:return
-    if words[0][0] == 'a':
-        if length != 5:return
-        WriteArc(True, words)
-    elif words[0][0] == 't':
-        if length != 5:return
-        WriteArc(False, words)
-    else:
-        if length != 2:return
-        WriteLine(words)
-    
-def WriteAreaCurve(shape_str):
-    length = len(shape_str)
-    i = 0
-    s = ""
-    HeeksCNC.program.python_program += "c = area.Curve()\n"
-    while i < length:
-        if shape_str[i] == '\n':
-            WriteSpan(s)
-            s = ""
-        else:
-            s += shape_str[i]
-        i = i + 1
-    HeeksCNC.program.python_program += "a.append(c)\n"
