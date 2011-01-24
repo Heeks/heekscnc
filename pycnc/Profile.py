1c14e99621beb0a401da0954a4e2813c7c5730a3
ddfalck2002 <ddfalck2002@yahoo.com>
2011-01-24 01:19:46 +0000

    added a rough draft of the Profile dialog

--- pycnc/Profile.py
+++ pycnc/Profile.py
@@ -1,4 +1,5 @@
 from Operation import Operation
+import HeeksCNC
 
 class Profile(Operation):
     def __init__(self):
@@ -10,3 +11,24 @@ class Profile(Operation):
     def op_icon(self):
         # the name of the PNG file in the HeeksCNC icons folder
         return "profile"
+
+    def get_selected_sketches(self):        
+        return 'hi'
+
+    def Edit(self):
+        #if HeeksCNC.widgets == HeeksCNC.WIDGETS_WX:
+            #from wxProfile import Profiledlg
+        import wxProfile
+        class wxProfiledlg( wxProfile.Profiledlg ):
+            def __init__( self, parent ):
+                wxProfile.Profiledlg.__init__( self, parent )
+
+                #r1 = self.m_roll_radius.GetValue()
+                #print r1
+
+            def CloseWindow(self,event):
+                self.Destroy()
+                
+
+        dlg = wxProfiledlg(None)
+        dlg.Show(True)
