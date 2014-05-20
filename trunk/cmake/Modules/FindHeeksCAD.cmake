fd1f96944b2be5a4d3006f06bc9c8903d7835332
neomilium <neomilium@f5ef57e9-3852-0410-a096-1f345c7b90ba>
2014-05-20 23:16:28 +0000

    CMake: improve FindHeeksCAD module
      * Use a cleaner method to find from multiple paths
      * Add "/usr/local/share/heekscad" to searched paths: HeeksCAD now install its sources there (with default prefix: /usr/local)
      * Add "/usr/share/heekscad" to searched paths: HeeksCAD now install its sources there (with most linux distributions prefix: /usr)

--- trunk/cmake/Modules/FindHeeksCAD.cmake
+++ trunk/cmake/Modules/FindHeeksCAD.cmake
@@ -9,19 +9,18 @@
 #
 
 IF(NOT HeeksCAD_FOUND)
-   # Will try to find at standard locations
-   FIND_PATH(HeeksCAD_SRC_DIR src/Geom.h PATH_SUFFIXES heekscad)
+  SET( SEARCH_PATHS
+    "/usr/local/share/heekscad"
+    "/usr/share/heekscad"
+    "${CMAKE_SOURCE_DIR}/heekscad"
+    "${CMAKE_SOURCE_DIR}/.."
+    "${CMAKE_SOURCE_DIR}/../heekscad"
+  )
+  
+  FIND_PATH( HeeksCAD_SRC_DIR interface/Geom.cpp PATHS ${SEARCH_PATHS} DOC "Path to HeeksCAD includes" )
+  IF( NOT HeeksCAD_SRC_DIR )
+      MESSAGE( FATAL_ERROR "Cannot find HeeksCAD sources dir." )
+  ENDIF( NOT HeeksCAD_SRC_DIR )
+  
+  MESSAGE( STATUS "HeeksCAD_SRC_DIR:     " ${HeeksCAD_SRC_DIR} )
 ENDIF(NOT HeeksCAD_FOUND)
-
-MESSAGE( STATUS "HeeksCAD_SRC_DIR:     " ${HeeksCAD_SRC_DIR} )
-
-IF( HeeksCAD_SRC_DIR STREQUAL HeeksCAD_SRC_DIR-NOTFOUND )
-  # try to find at heekscad/ location then ../
-  FIND_PATH( HeeksCAD_SRC_DIR src/Geom.h PATHS "${CMAKE_SOURCE_DIR}/heekscad" "${CMAKE_SOURCE_DIR}/.." "${CMAKE_SOURCE_DIR}/../heekscad"DOC "Path to HeeksCAD includes" )
-  #FIND_PATH( HeeksCAD_SRC_DIR src/Geom.h PATHS "${CMAKE_SOURCE_DIR}/heekscad" DOC "Path to HeeksCAD includes" )
-  IF( HeeksCAD_SRC_DIR STREQUAL Geom.h-NOTFOUND )
-    MESSAGE( FATAL_ERROR "Cannot find HeeksCAD sources dir." )
-   ENDIF( HeeksCAD_SRC_DIR STREQUAL Geom.h-NOTFOUND )
-ENDIF(HeeksCAD_SRC_DIR STREQUAL HeeksCAD_SRC_DIR-NOTFOUND )
-
-MESSAGE( STATUS "HeeksCAD_SRC_DIR:     " ${HeeksCAD_SRC_DIR} )
